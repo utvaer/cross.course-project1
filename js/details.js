178d70094e084a3b298e5a29a58510b16cd8401f
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2021-10-04 14:23:15 +0200

    homepage api call, overall styling, js tweaks, responsiveness

--- js/details.js
+++ js/details.js
@@ -10,15 +10,15 @@ const detailsUrl = "https://linnscape.com/wordpress/wp-json/wc/store/products/" 
 async function getDetails(url){
     const response = await fetch(url);
     const product = await response.json();
-    detailsContainer.innerHTML +=
+    
+    document.title = `Re-CyCle | ${product.name}`;
+    detailsContainer.innerHTML =
     `<div class="tour">
         <h2>${product.name}</h2>
         <img class="tour-image" src="${product.images[0].src}">
         <div class="tour-info">
             <p>${product.description}</p>
-            <div>Price: ${product.price}.- CHF</div>
-            <div> Departure: ${product.time}</div>
-            <div> Distance: ${product.distance}</div>
+            <div>Price: ${product.price_html}</div>
         </div>
         <button class="tour-button" data-tour="${product.id}">Book</button>
     </div>`
